a22a983500e2227062abcffa9c7a647733660b31
weiliams596 <mhy596520@gmail.com>
2024-11-04 15:51:45 +0500

    Add week10 files

---
+++ Week10/2024_11_4/JS/Operators_And_Expressions.js
@@ -0,0 +1,33 @@
+let a = 10, b = 12;
+
+//  == operator
+console.log(a == b); // false
+
+// === operator
+console.log(a === b); // false
+
+//!= operator
+console.log(a!= b); // true
+
+//!== operator
+console.log(a!== b); // true
+
+// > operator
+console.log(a > b); // false
+
+// < operator
+console.log(a < b); // true
+
+// >= operator
+console.log(a >= b); // false
+
+// <= operator
+console.log(a <= b); // true
+
+let age=17;
+
+if(age>=18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible to vote");
+}
