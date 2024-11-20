7faa549c3f218887c96997dac070eccbf4618e8d
weiliams596 <mhy596520@gmail.com>
2024-11-20 21:31:16 +0500

    Submit 20.11.2024 homework

---
+++ Week12/2024_11_20/JS/script.js
@@ -0,0 +1,44 @@
+// This is a JavaScript file
+
+
+function KZTToEUR(param){
+    const translater = 0.002;
+    return param * translater;
+}
+
+function USDToEUR(param){
+    const translater = 0.85;
+    return param * translater;
+}
+
+function EURToEUR(param){
+    return param;
+}
+
+function cashTranslater(){
+    let sum=0;
+    while(true){
+        let numb=+prompt("Enter the amount to translate (in KZT, USD or EUR):");
+        if(isNaN(numb) || numb<=0){
+            break;
+        }else {
+            switch(prompt("Enter the currency to translate to (KZT, USD or EUR):").toUpperCase()){
+                case "KZT":
+                    sum+=KZTToEUR(numb);
+                    break;
+                case "USD":
+                    sum+=USDToEUR(numb);
+                    break;
+                case "EUR":
+                    sum+=EURToEUR(numb);
+                    break;
+                default:
+                    alert("Invalid currency!");
+                    continue;
+            }
+        }
+    }
+    alert('Total sum: ' + sum + ' EUR');
+}
+
+cashTranslater();
