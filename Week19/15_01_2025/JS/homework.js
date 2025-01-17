9e316810830588a37dc699d7c42694714b2a60ef
unknown <your.email@example.com>
2025-01-17 21:33:12 +0500

    Submit 15.01.2025 homework

--- Week19/15_01_2025/JS/homework.js
+++ Week19/15_01_2025/JS/homework.js
@@ -4,32 +4,50 @@ const userList = document.querySelector('.user-list');
 
 const userData = [];
 
-const init = () => { 
-    fetch("https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/users")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        
-        userData.push(...data);
-        userData.forEach(user => {
-            const newDiv=document.createElement('div');
-            newDiv.classList.add('user-info-card');
-            newDiv.innerHTML = `
+const makeUserCard = (user) => {
+    const newDiv = document.createElement('div');
+                newDiv.classList.add('user-info-card');
+                newDiv.innerHTML = `
                 <img src="${user.image}">
                 <h2>Name:${user.name}</h2>
                 <p>Post:${user.post}</p>`;
-        });
-    })
-    .catch(error => console.error('Error:', error));
+                userList.appendChild(newDiv);
+}
+
+const makeUserList = (data) =>{
+    data.forEach(user => {
+        makeUserCard(user);
+    });
+}
+
+const init = () => {
+    fetch("https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/users")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+
+            userData.push(...data);
+            makeUserList(userData);
+        })
+        .catch(error => console.error('Error:', error));
 };
 
 searchButton.addEventListener('click', (e) => {
-
+    const inputValue = searchInput.value.toLowerCase();
+    let filteredData;
+    if(inputValue =='' || inputValue ==undefined){
+        filteredData = userData;
+    }
+    filteredData = userData.filter(user => {
+        return user.name.toLowerCase().includes(inputValue) || user.post.toLowerCase().includes(inputValue);
+    });
+    userList.innerHTML = '';
+    makeUserList(filteredData);
 });
 
 
