a6a91c82bc0b2e1bbeda0604cd685e38435cdbed
unknown <your.email@example.com>
2025-01-17 22:01:23 +0500

    Submit 16.01.2025 homework

--- Week19/16_01_2025/JS/homework.js
+++ Week19/16_01_2025/JS/homework.js
@@ -22,21 +22,37 @@ const makeUserList = (data) =>{
     });
 }
 
-const init = () => {
-    fetch("https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/users")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
+// const init = () => {
+//     fetch("https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/users")
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
 
-            userData.push(...data);
-            makeUserList(userData);
-        })
-        .catch(error => console.error('Error:', error));
+//             userData.push(...data);
+//             makeUserList(userData);
+//         })
+//         .catch(error => console.error('Error:', error));
+// };
+
+const init = async () => {
+    try{
+        const newFetch = await fetch("https://67873274c4a42c916105d2fe.mockapi.io/api/onlineduken/users");
+        if(!newFetch.ok){
+            throw  new Error('Network response was not ok');
+        }
+        console.log(newFetch);
+        const newData = await newFetch.json();
+        userData.push(...newData);
+        makeUserList(userData);
+    }
+    catch(error){
+        console.error('Error:', error);
+    }
 };
 
 searchButton.addEventListener('click', (e) => {
